d1c41fc12fb65316f58659fd5e5952df9c9a6454
HermanDhillon <dhillonherman1991@gmail.com>
2024-02-20 20:26:04 -0800

    add scan

--- src/components/BarcodeScanner.jsx
+++ src/components/BarcodeScanner.jsx
@@ -1,16 +1,21 @@
-import { useMediaDevices } from "react-media-devices";
+import { useState } from "react";
 import { useZxing } from "react-zxing";
 import Webcam from "react-webcam";
 
 function BarcodeScanner() {
+  const [result, setResult] = useState("");
+  const { ref } = useZxing({
+    onDecodeResult(result) {
+      setResult(result.getText());
+    },
+  });
   return (
     <div>
-      <Webcam
-        audio={false}
-        height={720}
-        screenshotFormat="image/jpeg"
-        width={1280}
-      />
+      <Webcam audio={false} height={720} width={1280} ref={ref} />
+      <p>
+        <span>Last result:</span>
+        <span>{result}</span>
+      </p>
     </div>
   );
 }
