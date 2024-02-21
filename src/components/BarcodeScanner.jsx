43f51bc101d1ad795a4c50d723438be2f747dc33
HermanDhillon <dhillonherman1991@gmail.com>
2024-02-20 20:13:26 -0800

    remove media devices library, add react-webcam library

--- src/components/BarcodeScanner.jsx
+++ src/components/BarcodeScanner.jsx
@@ -5,7 +5,12 @@ import Webcam from "react-webcam";
 function BarcodeScanner() {
   return (
     <div>
-      <Webcam />
+      <Webcam
+        audio={false}
+        height={720}
+        screenshotFormat="image/jpeg"
+        width={1280}
+      />
     </div>
   );
 }
