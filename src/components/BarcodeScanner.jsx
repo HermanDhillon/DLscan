aa3ba12229213ca02de16f1ea0b72a0081169f2d
hermandhillon <dhillonherman1991@gmail.com>
2024-02-21 00:11:22 -0800

    install quagga

--- src/components/BarcodeScanner.jsx
+++ src/components/BarcodeScanner.jsx
@@ -1,17 +1,36 @@
-import { useState } from "react";
-import { useZxing } from "react-zxing";
+import { useState, useEffect } from "react";
+import Quagga from "quagga";
 
 function BarcodeScanner() {
   const [result, setResult] = useState("");
-  const { ref } = useZxing({
-    onDecodeResult(result) {
-      setResult(result.getText());
-    },
-  });
+
+  useEffect(() =>
+    Quagga.init(
+      {
+        locate: true,
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: document.querySelector("#webcam"), // Or '#yourElement' (optional)
+        },
+        decoder: {
+          readers: ["code_128_reader"],
+        },
+      },
+      function (err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+      }
+    )
+  );
 
   return (
     <>
-      <video ref={ref} />
+      <video id="webcam" />
       <p>
         <span>Last result:</span>
         <span>{result}</span>
