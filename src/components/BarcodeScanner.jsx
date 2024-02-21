f3a9fec49c1c698937ac3ecda964d2cffbdbb4be
hermandhillon <dhillonherman1991@gmail.com>
2024-02-20 22:14:08 -0800

    use native media api

--- src/components/BarcodeScanner.jsx
+++ src/components/BarcodeScanner.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useZxing } from "react-zxing";
-import Webcam from "react-webcam";
 
 function BarcodeScanner() {
   const [result, setResult] = useState("");
@@ -9,15 +8,15 @@ function BarcodeScanner() {
       setResult(result.getText());
     },
   });
+
   return (
-    <div>
-      <Webcam audio={false} height={720} width={1280} ref={ref} />
+    <>
+      <video ref={ref} />
       <p>
         <span>Last result:</span>
         <span>{result}</span>
       </p>
-    </div>
+    </>
   );
 }
-
 export default BarcodeScanner;
