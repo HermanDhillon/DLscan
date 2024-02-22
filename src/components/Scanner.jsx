abe467c8e6191f9f274fecb3ca0c132d9e6a52ed
hermandhillon <dhillonherman1991@gmail.com>
2024-02-21 18:18:30 -0800

    add pdf417 parser library

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,11 +1,12 @@
 import { useZxing } from "react-zxing";
+const { parse } = require("driver-license-parser");
 
 function Scanner(props) {
   const { setResult } = props;
   const { ref } = useZxing({
     onDecodeResult(result) {
       setResult(result.getText());
-      console.log("got result");
+      console.log(parse(result.getText()));
     },
   });
 
